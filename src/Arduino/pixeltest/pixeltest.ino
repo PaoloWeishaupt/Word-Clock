71d695c26c7b4a4fbd10f31bbba9edf539c4d027
PaoloWeishaupt <37296192+PaoloWeishaupt@users.noreply.github.com>
2019-03-13 15:03:46 +0100

    Rtc e LED aggiunta

--- src/Arduino/pixeltest/pixeltest.ino
+++ src/Arduino/pixeltest/pixeltest.ino
@@ -130,43 +130,93 @@ void sonoLeQuattro() {
     pixelOn(pixels[5 + i][9], green);
   }
 }
-}
 
 void printTime(int hour, int minute, int second){
-   if(hour != 1 && hour != 13){
-     pixelOn(pixels[1][2], red);
-     pixelOn(pixels[1][3], red);
-     pixelOn(pixels[1][4], red);
-     pixelOn(pixels[1][5], red);
-     pixelOn(pixels[1][7], red);
-     pixelOn(pixels[1][8], red);
-      time += "Sono le ";
-    if(hour == 2 || hour == 14){
-        time += "due ";
-    }else if(hour == 3 || hour == 15){
-        time += "tre ";
-    }else if(hour == 4 || hour == 16){
-        time += "quattro ";
-    }else if(hour == 5 || hour == 17){
-        time += "cinque ";
-    }else if(hour == 6 || hour == 18){
-        time += "sei ";
-    }else if(hour == 7 || hour == 19){
-        time += "sette ";
-    }else if(hour == 8 || hour == 20){
-        time += "otto ";
-    }else if(hour == 9 || hour == 21){
-        time += "nove ";
-    }else if(hour == 10 || hour == 22){
-        time += "dieci ";
-    }else if(hour == 11 || hour == 23){
-        time += "undici ";
-    }else if(hour == 12 || hour == 24){
-        time += "dodici ";
-    }
-   }
-   else{
-      time += "È la una";
+  if(hour != 1 && hour != 13){
+    pixelOn(pixels[1][2], red);
+    pixelOn(pixels[1][3], red);
+    pixelOn(pixels[1][4], red);
+    pixelOn(pixels[1][5], red);
+    pixelOn(pixels[1][7], red);
+    pixelOn(pixels[1][8], red);
+    //time += "Sono le ";
+  if(hour == 2 || hour == 14){
+    pixelOn(pixels[2][12], red);
+    pixelOn(pixels[2][13], red);
+    pixelOn(pixels[2][14], red);
+    //time += "due ";
+  }else if(hour == 3 || hour == 15){
+    pixelOn(pixels[3][1], red);
+    pixelOn(pixels[3][2], red);
+    pixelOn(pixels[3][3], red);
+    //time += "tre ";
+  }else if(hour == 4 || hour == 16){
+    pixelOn(pixels[3][5], red);
+    pixelOn(pixels[3][6], red);
+    pixelOn(pixels[3][7], red);
+    pixelOn(pixels[3][8], red);
+    pixelOn(pixels[3][9], red);
+    pixelOn(pixels[3][10], red);
+    pixelOn(pixels[3][11], red);
+    //time += "quattro ";
+  }else if(hour == 5 || hour == 17){
+    pixelOn(pixels[4][1], red);
+    pixelOn(pixels[4][2], red);
+    pixelOn(pixels[4][3], red);
+    pixelOn(pixels[4][4], red);
+    pixelOn(pixels[4][5], red);
+    pixelOn(pixels[4][6], red);
+    //time += "cinque ";
+  }else if(hour == 6 || hour == 18){
+    pixelOn(pixels[3][12], red);
+    pixelOn(pixels[3][13], red);
+    pixelOn(pixels[3][14], red);
+    //time += "sei ";
+  }else if(hour == 7 || hour == 19){
+    pixelOn(pixels[4][10], red);
+    pixelOn(pixels[4][11], red);
+    pixelOn(pixels[4][12], red);
+    pixelOn(pixels[4][13], red);
+    pixelOn(pixels[4][14], red);
+    //time += "sette ";
+  }else if(hour == 8 || hour == 20){
+    pixelOn(pixels[2][12], red);
+    pixelOn(pixels[2][13], red);
+    pixelOn(pixels[2][14], red);
+    pixelOn(pixels[2][12], red);
+    //time += "otto ";
+  }else if(hour == 9 || hour == 21){
+    pixelOn(pixels[2][12], red);
+    pixelOn(pixels[2][13], red);
+    pixelOn(pixels[2][14], red);
+    pixelOn(pixels[2][12], red);
+    //time += "nove ";
+  }else if(hour == 10 || hour == 22){
+    pixelOn(pixels[2][12], red);
+    pixelOn(pixels[2][13], red);
+    pixelOn(pixels[2][14], red);
+    pixelOn(pixels[2][12], red);
+    pixelOn(pixels[2][13], red);
+    //time += "dieci ";
+  }else if(hour == 11 || hour == 23){
+    pixelOn(pixels[2][12], red);
+    pixelOn(pixels[2][13], red);
+    pixelOn(pixels[2][14], red);
+    pixelOn(pixels[2][12], red);
+    pixelOn(pixels[2][13], red);
+    pixelOn(pixels[2][14], red);
+    //time += "undici ";
+  }else if(hour == 12 || hour == 24){
+    pixelOn(pixels[2][12], red);
+    pixelOn(pixels[2][13], red);
+    pixelOn(pixels[2][14], red);
+    pixelOn(pixels[2][12], red);
+    pixelOn(pixels[2][13], red);
+    pixelOn(pixels[2][14], red);
+    //time += "dodici ";
+  }
+} else{
+    time += "È la una";
    }
    return time;
 }
@@ -218,4 +268,3 @@ void randPixel() {
   int randJ = random(15);
   pixelOn(pixels[randI][randJ], c);
 }
->>>>>>> before discard
