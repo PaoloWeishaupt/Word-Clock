3eaebc342da184bdea27471e11d4558ddb7e22d4
gabrialessi <gabrialessi23@gmail.com>
2019-05-03 16:25:16 +0200

    fix buttontest - setNum with pos

--- src/Arduino/buttontest/buttontest.ino
+++ src/Arduino/buttontest/buttontest.ino
@@ -1,9 +1,60 @@
-#include <SevSeg.h>
+int a = 2;
+int b = 3;
+int c = 4;
+int d = 5;
+int e = 6;
+int f = 7;
+int g = 8;
+int D1 = 9;
+int D2 = 10;
+int D3 = 11;
+int D4 = 12;
+
+byte digit[10][8] =
+{
+  { 1, 1, 1, 1, 1, 1, 0, 0 }, // 0
+  { 0, 1, 1, 0, 0, 0, 0, 0 }, // 1
+  { 1, 1, 0, 1, 1, 0, 1, 0 }, // 2
+  { 1, 1, 1, 1, 0, 0, 1, 0 }, // 3
+  { 0, 0, 1, 0, 0, 1, 1, 0 }, // 4
+  { 1, 0, 1, 1, 0, 1, 1, 0 }, // 5
+  { 1, 0, 1, 1, 1, 1, 1, 0 }, // 6
+  { 1, 1, 1, 0, 0, 0, 0, 0 }, // 7
+  { 1, 1, 1, 1, 1, 1, 1, 0 }, // 8
+  { 1, 1, 1, 0, 0, 1, 1, 0 }  // 9
+};
 
 void setup() {
-  
+  pinMode(a, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(d, OUTPUT);
+  pinMode(e, OUTPUT);
+  pinMode(f, OUTPUT);
+  pinMode(g, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, LOW);
+  digitalWrite(D3, LOW);
+  digitalWrite(D4, LOW);
 }
 
 void loop() {
-  
+  setNum(0, 5);
+}
+
+void setNum(int pos, int N)
+{
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2, HIGH);
+  digitalWrite(D3, HIGH);
+  digitalWrite(D4, HIGH);
+
+  for (int i = 0 ; i < 8 ; i++) {
+    digitalWrite(i + 2 , digit[N][i]);
+    digitalWrite(pos + 9, LOW);
+  }
 }
